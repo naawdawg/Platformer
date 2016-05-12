1ba047b397be282683e9e1226f612d4873c63461
runejunior <daltond@shaw.ca>
2016-05-11 19:31:53 -0700

    Add files via upload

---
+++ platformer/background2.php
@@ -0,0 +1,2 @@
+   <a id="bgndVideo" class="player" data-property="{videoURL:'https://www.youtube.com/watch?v=ouoNk2dzNwM',containment:'body',autoPlay:true, mute:false, startAt:15, opacity:1}"></a>
+   <div class="pattern"></div> 
