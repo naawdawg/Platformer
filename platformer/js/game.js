108500228403401bcea2d1e4138f771b2afa828a
Kaymit <KFMitch@gmail.com>
2016-05-12 15:24:31 -0700

    Add files via upload
    Changed walking animation to every 5th frame

--- platformer/js/game.js
+++ platformer/js/game.js
@@ -16,16 +16,18 @@ bgImage.onload = function () {
 bgImage.src = "images/background.jpg";
 
 // sprite image
+var Timer = null;
+var imageNum = 0;
 var spriteReady = false;
 var spriteImage = new Image();
 spriteImage.onload = function () {
 	spriteReady = true;
 };
-spriteImage.src = "images/walk1.gif";
+spriteImage.src = "images/Walk0.gif";
 
 // Game objects
 var sprite = {
-	speed: 256 // movement in pixels per second
+	speed: 150 // movement in pixels per second
 };
 
 // Handle keyboard controls
@@ -45,60 +47,129 @@ var reset = function () {
 	sprite.y = canvas.height / 2;
 };
 
-// Update game objects
-var gravity = 3;
+//animation of sprite
+var animationStep = 0;
+
+function move() {
+	if(39 in keysDown) {//right
+		animationStep++;
+		if(animationStep = 4) {
+			imageNum = (imageNum + 1) % 4;
+			spriteImage.src = "images/Walk" + imageNum + ".gif";
+			animationStep = 0;
+		}
+	}
+	if(37 in keysDown) {//left
+		animationStep++;
+		if(animationStep = 4) {
+			imageNum = (imageNum + 1) % 4;
+			spriteImage.src = "images/WalkL" + imageNum + ".gif";
+			animationStep = 0;
+		}
+	}
+	if(38 in keysDown && 39 in keysDown) {//up right
+		spriteImage.src = "images/Jump1.gif";
+	}
+	if(38 in keysDown && 37 in keysDown) {//up left
+		spriteImage.src = "images/JumpL1.gif";
+	}
+	animationStep = 0;
+};
+
+var gravity = 3.5;
 var jumpAvailable = false;
 var jumping = false;
 var jumpMax = 5;
-var jumpVelocity = 2;
+var jumpVel = 2;
 
+// Update game objects
 var update = function (modifier) {
-	if (sprite.y == canvas. height - spriteH) {
+	if(sprite.y == canvas.height - spriteH) {
 		jumpAvailable = true;
-	} else {
-		jumpAvailable = false
 	}
-	
-	if (jumping) {
-		sprite.y -= jumpVelocity;
-		jumpVelocity -= 0.1;
-		if (jumpVelocity <= 0) {
+	else {
+		jumpAvailable = false;
+	}
+
+	if(jumping) {
+		sprite.y -= jumpVel;
+		jumpVel -= 0.1;
+		if(jumpVel <= 0) {
 			jumping = false;
-			jumpAvailable = true;
+			if(!jumping) {
+				if(39 in keysDown || 38 in keysDown) {
+					spriteImage.src = "images/Jump2.gif";
+				}
+				spriteImage.src = "images/Jump2.gif";
+			}
 		}
- 	} else {
+	}
+	else {
 		sprite.y += gravity;
 	}
+
 	if (38 in keysDown) { // Player holding up
 		//sprite.y -= sprite.speed * modifier;
-		if (jumpAvailable) {
+
+		if(jumpAvailable) {
 			jumping = true;
-			jumpVelocity = jumpMax;
+			jumpVel = jumpMax;
+		}
+
+		if(Timer == null) {
+			Timer = setInterval('move();', 10);
+		}
+		else {
+			clearInterval(Timer);
+			Timer = null;
 		}
 	}
 	if (40 in keysDown) { // Player holding down
 		sprite.y += sprite.speed * modifier;
+		if(Timer == null) {
+			Timer = setInterval('move();', 10);
+		}
+		else {
+			clearInterval(Timer);
+			Timer = null;
+		}
 	}
 	if (37 in keysDown) { // Player holding left
 		sprite.x -= sprite.speed * modifier;
+		if(Timer == null) {
+			Timer = setInterval('move();', 10);
+		}
+		else {
+			clearInterval(Timer);
+			Timer = null;
+		}
 	}
 	if (39 in keysDown) { // Player holding right
 		sprite.x += sprite.speed * modifier;
+		if(Timer == null) {
+			Timer = setInterval('move();', 10);
+		}
+		else {
+			clearInterval(Timer);
+			Timer = null;
+		}
 	}
-	
-	if (sprite.x < 0) {
+
+	if(sprite.x < 0) {
 		sprite.x = 0;
 	}
-	if (sprite.y < 0) {
+	if(sprite.y < 0) {
 		sprite.y = 0;
 	}
-	if (sprite.x > canvas.width - spriteW) {
-		sprite.x = canvas.width - spriteW; 
+	if(sprite.x > canvas.width - spriteW) {
+		sprite.x = canvas.width - spriteW;
 	}
-	if (sprite.y > canvas.height - spriteH) {
-		sprite.y = canvas. height - spriteH;
+	if(sprite.y > canvas.height - spriteH) {
+		sprite.y = canvas.height - spriteH;
 	}
 };
+
+window.onkeydown = move;
 
 // Draw everything
 var render = function () {
