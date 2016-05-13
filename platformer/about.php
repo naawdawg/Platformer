1dc77d6575246689df6f451770abfd7f002cd7f5
runejunior <daltond@shaw.ca>
2016-05-12 23:58:16 -0700

    Update about.php

--- platformer/about.php
+++ platformer/about.php
@@ -2,13 +2,92 @@
 <section class="big-background">
    <?php include("background2.php"); ?>
    <div class="big-background-container">
-      <h1 class="big-background-title" style="margin: 0">The Team</h1>
+      <h1 class="big-background-title" style="margin: 0; margin-bottom: 40px">The Team</h1>
+      <img src="images/Logo.png" style="height: 100%; width: 100%; opacity: 0.4; margin-bottom: 20px">
       
+
       
-      <h1 id="colorize" style="margin: 0; margin-top: 20px">Developed by Group:</h1>
-      <img src="images/Logo.png" style="height: 100%; width: 100%; opacity: 0.4">
       
       
    </div>                                                    
 </section>
+<div class="wrapper">
+   <section class="small-background-section">
+      <div class="pattern"></div>
+      <div class="small-background-container col-xs-12">
+         <div class="col-xs-12">
+            <div class="col-md-6">        
+               <img src="images/Logo.png" style="height: 80%; width: 80%;  max-width: 500px; opacity: 0.4; margin: 30px 0px">
+               <h1 id="colorize" style="margin: 0; margin-bottom: 0px">Dalton Danis</h1>
+               <p style="color: white">
+                  A comedian and programmer on the side, He is an exceptional individual
+                  who has taken upon many project, for example this website. Dalton plays
+                  a variety of games Diablo 3, Gmod, Minecraft, COD, and etc...  He prefers
+                  programming in Java, C++, Php, and MySQL. On his off time he enjoys
+                  playing hockey and watching it too. Back in the day when he was a young
+                  lad, he developed his skills in C++ and spending many hours working in his
+                  father’s IT business.
+               </p>
+            </div>
+            <div class="col-md-6">
+               <img src="images/Logo.png" style="height: 80%; width: 80%; max-width: 500px; opacity: 0.4; margin: 30px 0px">
+               <h1 id="colorize" style="margin: 0; margin-bottom: 0px">Kevin Mitchell</h1>
+               <p style="color: white">
+                  Having undertaken a few computing projects earlier in life, Platformer
+                  is Kevin’s first major development project. Coming from a background of
+                  extremely diverse subjects, he has undertaken the role of communications
+                  specialist and software developer. He currently has an excellent relationship
+                  with Java, and is starting to get pretty close to JS and HTML5. He’s
+                  currently recovering from a background of Diablo and Warcraft, and now spends
+                  more of his time crafting and gathering useful information and skills. 
+               </p>
+            </div>
+         </div>
+         
+         
+         <div class="col-xs-12">
+            <div class="col-md-6">        
+               <img src="images/Logo.png" style="height: 80%; width: 80%;  max-width: 500px; opacity: 0.4; margin: 30px 0px">
+               <h1 id="colorize" style="margin: 0; margin-bottom: 0px">Vinz Balitaan</h1>
+               <p style="color: white">
+                  Vinz has experience playing many of the popular modern computer games,
+                  but less so developing games like them, and this will be his entrance
+                  into games development. Like many, he enjoys taking advantage of Java’s
+                  intuitive properties and practices. Outside of academics he loves being
+                  active and hitting the gym.
+               </p>
+            </div>
+            <div class="col-md-6">
+               <img src="images/Logo.png" style="height: 80%; width: 80%; max-width: 500px; opacity: 0.4; margin: 30px 0px">
+               <h1 id="colorize" style="margin: 0; margin-bottom: 0px">Renz Dionglay</h1>
+               <p style="color: white">
+                  Renz is all about gaming - a logical choice for Platformer’s user
+                  experience expert. He loves mostly PC games like Diablo 3, CSGO
+                  Summoner’s War, and many, many more.
+               </p>
+            </div>
+         </div>
+         
+         
+         <div class="col-xs-12">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+               <img src="images/Logo.png" style="height: 80%; width: 80%;  max-width: 500px; opacity: 0.4; margin: 30px 0px">
+               <h1 id="colorize" style="margin: 0; margin-bottom: 0px">Niko Lauron</h1>
+               <p style="color: white">
+                  Niko loves gaming on numerous platforms (heh) like the Playstation
+                  environment, Nintendo’s gaming systems, and PCs to top it off. This
+                  is also his largest software project, and his primary area of expertise
+                  in Platformer is level design and integration.
+               </p>
+            </div>
+            <div class="col-md-3"></div>
+         </div>
+         
+         
+
+
+      </div>
+   </section>
+</div>
 <?php include("footer.php"); ?>
