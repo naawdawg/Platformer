e423be84015d13583b0503beb33c5f5e003b47bc
RenzD <renzdionglay@gmail.com>
2016-05-24 15:03:41 -0700

    Add files via upload

--- platformer2/menu/gameview.php
+++ platformer2/menu/gameview.php
@@ -1,20 +1,13 @@
 <div id="Game" class="div-container center">
+
 <canvas id="canvas"></canvas>
 <script src = "js/game.js"></script>
-   <div class="col-xs-12">
-      <div class="col-xs-3" >
-         <button id="Right" class="mobile-button">RIGHT</button>
-      </div>
-      <div class="col-xs-3">
-         <button id="Left" class="mobile-button">LEFT</button>
-      </div>
-      <div class="col-xs-6">
-         <button id="Up" class="mobile-button">SPACE</button>
-      </div>
-   </div>
-<div class="col-xs-12 center"><button class="show_b">Back</button></div>
+	<button class="bottomLeft" id="Left">LEFT</button>
+	<button id="Right">RIGHT</button>
+	<button id="Up">SPACE</button>
+	<button id="Pause">PAUSE</button>
+	<button id="back" class="show_b">Back</button>
 </div>
-
 
 <script>
 $(document).ready(function(){
@@ -83,9 +76,5 @@ $(document).ready(function(){
          }
       }
    );
-   
-   
-   
-
 });
 </script>
