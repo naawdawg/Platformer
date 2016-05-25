8746758105edd39209d8a79405b80da6e14342bf
RenzD <renzdionglay@gmail.com>
2016-05-25 02:02:10 -0700

    Update menu.php

--- platformer2/menu.php
+++ platformer2/menu.php
@@ -25,13 +25,170 @@ $(document).ready(function(){
    });
    
    
+   //==========================================================
    //BUTTON LEVEL SELECT FOR LEVEL 1
    $("#show_g").click(function(){
 	  //ADD ACTION LISTENERS HERE
-	   reset();
+	  mapLevel = 1;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
       $("#Level").hide();
       $("#Game").show();
    }); 
+   
+   $("#show_g2").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 2;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g3").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 3;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+
+   $("#show_g4").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 4;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g5").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 5;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+
+   $("#show_g6").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 6;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g7").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 7;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+
+   $("#show_g8").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 8;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g9").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 9;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+
+   $("#show_g10").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 10;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g11").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 11;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+
+   $("#show_g12").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 12;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+
+   $("#show_g13").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 13;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g14").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 14;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+
+   $("#show_g15").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 15;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   $("#show_g16").click(function(){
+	  //ADD ACTION LISTENERS HERE
+	  mapLevel = 16;
+	  gameBool = true;
+	  pauseBool = false;
+	  reset();
+      $("#Level").hide();
+      $("#Game").show();
+   }); 
+   
+   //==========================================================
    
    $("#show_s").click(function(){
       $("#Main").hide();
