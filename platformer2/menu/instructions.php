1a6b13f035ba7043df40ca9d1bd8270edfa8a773
vinzbalitaan <vinzbalitaan@hotmail.com>
2016-05-25 00:18:45 -0700

    Update instructions.php

--- platformer2/menu/instructions.php
+++ platformer2/menu/instructions.php
@@ -1,4 +1,62 @@
-<div id="Instruction" class="div-container center">
-Instructions
+<style>
+    .carousel-inner > .item > img {
+    margin: 0 auto;
+}
+</style>
+<body>
+    <div id="Instruction">
+    <br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="images/rightInstruct.png" alt="Right" margin = "100px">
+            <p>Click the right button on the screen or the</p>
+            <p>right arrow key to move to the right.</p>
+        </div>
+
+        <div class="item">
+            <img src="images/leftInstruct.png" alt="Left" margin-left="100px" margin-right="100px">
+            <p>Click the left button on the screen or the</p>
+            <p>left arrow key to move to the left.</p>
+        </div>
+    
+        <div class="item">
+            <img src="images/jumpInstruct.png" alt="Jump" margin-left="100px" margin-right="100px">
+            <p>Click the space button on the screen or the</p>
+            <p>up arrow key to jump up.</p>
+        </div>
+       
+        <div class="item">
+            <img src="images/pause.png" alt="Right" margin = "100px">
+            <p>To pause, click on the pause button on the top left or the</p>
+            <p>"X" button key.</p>
+        </div>
+
+        <div class="item">
+            <img src="images/iceInstruct.png" alt="Ice Destruction" margin-left="100px" margin-right="100px">
+            <p>Destroy all ice blocks to complete the level.</p>
+            <p>To destroy ice blocks, walk on top of them.</p>
+            <p>Once all ice blocks are destroyed, the crystal image will turn into a sign.</p>
+        </div>
+        
+        <div class="item">
+            <img src="images/sign.png" alt="Right" margin = "100px">
+            <p>Once all ice blocks are destroyed, go to the sign to complete the level.</p>
+        </div>
+    </div>
+        
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 <div class="col-xs-12 center"><button class="show_b">Back</button></div>
 </div>
+</body>
